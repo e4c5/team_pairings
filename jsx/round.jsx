3004b5373eea66bdee7317250743aead36395d25
raditha <raditha.dissanayake@gmail.com>
2022-10-27 22:39:07 +0530

    Working on scores entry

--- jsx/round.jsx
+++ jsx/round.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, ButtonGroup, TextField }  from '@mui/material';
+import { Button, ButtonGroup, TextField,
+     Autocomplete, Box  
+}  from '@mui/material';
+
 import { Table, TableBody, TableCell,
          TableContainer, TableHead, TableRow } from '@mui/material';
 import {Paper, Tooltip} from '@mui/material';
-import { Link, Switch, Box } from '@mui/material';
+
 
 import {
     useParams,
@@ -14,11 +17,55 @@ import {
 import getCookie from './cookie.js';
 
 
+function Result({r, tournament}) {
+    const editable = document.getElementById('editable')
+
+    function resultIn() {
+        if(editable) {
+
+        }
+        return (
+            <TableRow>
+                <TableCell>{ r.first.name }</TableCell>
+                <TableCell>{ r.games_won } / { tournament.team_size - r.games_won }</TableCell>
+                <TableCell>{ r.score1 }</TableCell>
+                <TableCell>{ r.second.name }</TableCell>
+                <TableCell>{ tournament.team_size - r.games_won } / { r.games_won }</TableCell>
+                <TableCell>{ r.score1 }</TableCell>
+            </TableRow>
+        )
+    }
+
+    function resultOut() {
+        return (
+            <TableRow>
+                <TableCell>{ r.first.name }</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>{ r.second.name }</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+            </TableRow>
+        )
+    }
+
+    if(r.score1) {
+        return resultIn()
+    }
+    else {
+        return resultOut();
+    }
+}
 
 export function Round(props) {
     const params = useParams()
     const [round, setRound] = React.useState(null)
     const [results, setResults] = React.useState(null)
+    const [names, setNames] = React.useState([])
+    const [first, setFirst] = React.useState({})
+    const [second, setSecond] = React.useState({})
+    const [score1, setScore1] = React.useState('')
+    const [score2, setScore2] = React.useState('')
 
     useEffect(() => {
         if(round == null) {
@@ -29,7 +76,16 @@ export function Round(props) {
         else
         {
             if(results == null) {
-                fetch(`/api/${round.id}/result/`).then(resp => resp.json()).then(json => setResults(json))
+                fetch(`/api/${round.id}/result/`).then(resp => resp.json()
+                ).then(json => {
+                    setResults(json)
+                    const left = []
+                    json.forEach(e =>{
+                        left.push(e.first.name)
+                        left.push(e.second.name)
+                    })
+                    setNames(left)
+                })
             }
         }
     })
@@ -49,40 +105,76 @@ export function Round(props) {
         
     }
 
-    console.log(results)
+    function handleChange(e, name) {
+        if('name' == 'score1') {
+            setScore1(e.target.value)
+        }
+        if('name' == 'score2') {
+            setScore2(e.target.value)
+        }
+    }
+    function changeName(e, name) {
+        
+        results.forEach(result => {
+            if(name == result.first.name) {
+                setFirst(result.first)
+                setSecond(result.second)
+            }
+            if(name == result.second.name) {
+                setFirst(result.second)
+                setSecond(result.first)
+            }
+        })
+    }
+
     if(round?.paired && results) {
         const tournament = props.tournament
         return(
+        
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                     <TableRow>
-                        <TableCell>Rank</TableCell>
                         <TableCell align="right">Team 1</TableCell>
-                        <TableCell align="right">W/L</TableCell>
-                        <TableCell align="right">Round Wins</TableCell>
-                        <TableCell align="right">Spread</TableCell>
+                        <TableCell align="right">Wins</TableCell>
+                        <TableCell align="right">Total Score</TableCell>
                         <TableCell align="right">Team 2</TableCell>
-                        <TableCell align="right">W/L</TableCell>
-                        <TableCell align="right">Round Wins</TableCell>
-                        <TableCell align="right">Spread</TableCell>
+                        <TableCell align="right">Wins</TableCell>
+                        <TableCell align="right">Total Score</TableCell>
                         
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                        { results.map(r => (
-                            <TableRow key={r.id}>
-                                <TableCell>{ r.first.name }</TableCell>
-                                <TableCell>{ r.games_won } / { tournament.team_size - r.games_won }</TableCell>
-                                <TableCell>{ r.score1 }</TableCell>
-                                <TableCell>{ r.second.name }</TableCell>
-                                <TableCell>{ tournament.team_size - r.games_won } / { r.games_won }</TableCell>
-                                <TableCell>{ r.score1 }</TableCell>
+                        { names && (
+                            <TableRow>
+                                <TableCell>
+                                <Autocomplete options={names} freeSolo disableClearable
+                                    onChange={ (e, newvalue) => changeName(e, newvalue)}
+                                    renderInput={(params) => (
+                                            <TextField
+                                            {...params}
+                                            label="Team Name"
+                                            InputProps={{
+                                                ...params.InputProps,
+                                                type: 'search',
+                                            }}
+                                            />
+                                    )}/>
+                                </TableCell>
+                                <TableCell><TextField/></TableCell>
+                                <TableCell><TextField disabled value={second?.name}/></TableCell>
+                                <TableCell><TextField value={score1} placeholder="Score for team1"
+                                         onChange={ e => handleChange(e, 'score1')} /></TableCell>
+                                <TableCell><TextField/></TableCell>
+                                <TableCell><TextField/></TableCell>
                             </TableRow>
-                        ))}
+                        )}
+                        { results.map(r => <Result  key={r.id} r={r} tournament={tournament} />)}
                     </TableBody>
                 </Table>
             </TableContainer>
+            
+            
         )
     }
     else {
@@ -120,4 +212,4 @@ export function Rounds(props) {
     }
 }
 
-console.log('Rounds 0.01')
+console.log('Rounds 0.01.4mi')
