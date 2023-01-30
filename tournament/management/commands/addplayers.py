916ae96355e29065244a5ad3550f94d34d652e1e
raditha <raditha.dissanayake@gmail.com>
2023-01-30 22:41:27 +0530

    Hello faker!

--- tournament/management/commands/addplayers.py
+++ tournament/management/commands/addplayers.py
@@ -1,12 +1,38 @@
+import faker
 from django.core.management.base import BaseCommand, CommandError
 from tournament.models import Participant, Tournament
-from tournament.tools import add_players
+from tournament.tools import add_participants, add_team_members
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('--faker',action='store_true',
+            help='Use faker to generate data')
+        
+        parser.add_argument('--tournament', help="The name of the tournament")
+        parser.add_argument('--tournament_id', help="The id of the tournament")
+        parser.add_argument('--add-teams', type=int,
+                help='Add this many teams to the tournament')
+        parser.add_argument('--add-members', action='store_true')
+
+
     def handle(self, *args, **options):
-        t = Tournament.get_by_name("richmond scrabble showdown u20",'')
-        add_players("api/tests/data/teams.csv", t)
+        print(options)
+
+        if options.get('tournament'):
+            self.t = Tournament.get_by_name(options.get('tournament'),'')
+        else:
+            self.t = Tournament.objects.get(pk=options.get('tournament_id'))
+
+        if options.get('add_teams'):
+            if options.get('faker'):
+                add_participants(self.t, use_faker=True, count=options.get('add_teams'))
+            else:
+                t = Tournament.get_by_name("richmond scrabble showdown u20",'')
+                add_participants(self.t, False, filename="api/tests/data/teams.csv")
+
+        if options.get('add_members'):
+            add_team_members(self.t)
 
 
 
--- tournament/tools.py
+++ tournament/tools.py
@@ -1,10 +1,29 @@
 import csv
-from tournament.models import Participant
+from faker import Faker
 
-def add_players(filename, tournament):
-    with open(filename) as fp:
-        reader = csv.reader(fp)
+from tournament.models import Participant, TeamMember
 
-        for line in reader:
-            Participant.objects.create(tournament=tournament, name=line[0],
-                seed=line[1])
+def add_participants(tournament, use_faker=False, count=0, filename=""):
+    if use_faker:
+        fake = Faker()
+        for _ in range(count):
+            Participant.objects.create(tournament=tournament, 
+                name=fake.city() + " Scrabble Club",
+                seed=fake.random_int(0, 1000))
+
+    else:
+        with open(filename) as fp:
+            reader = csv.reader(fp)
+
+            for line in reader:
+                Participant.objects.create(tournament=tournament, name=line[0],
+                    seed=line[1])
+
+
+def add_team_members(tournament):
+    fake = Faker()
+
+    for participant in tournament.participants.all():
+        for i in range(tournament.team_size):
+            TeamMember.objects.create(team=participant, board=i+1,
+                name=fake.name(), wins=0, spread=0)
