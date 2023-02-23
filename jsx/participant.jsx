5e2ce439b0d46a419f99130809650ab8a94f94d6
raditha <raditha.dissanayake@gmail.com>
2023-02-23 12:07:23 +0530

    Introdution to sorting

--- jsx/participant.jsx
+++ jsx/participant.jsx
@@ -11,8 +11,21 @@ import getCookie from './cookie.js';
 export function Participants(props) {
     const tournament = useTournament();
     const dispatch = useTournamentDispatch();
-    
+    const [order, setOrder] = useState('pos')
+
     const auth = document.getElementById('hh') && document.getElementById('hh').value
+    const columns = [
+        ["Rank", "pos"],
+        ["Name", "name"],
+        ["Rating", "rating"],
+        ["Round Wins", "round_wins"],
+        ["Game Wins", "game_wins"],
+        ["Spread", "spread"],
+    ]
+
+    if (auth) {
+        columns.push(["Actions"])
+    }
 
     /**
      * Switch the on / off state of a participant.
@@ -24,15 +37,16 @@ export function Participants(props) {
         const p = tournament.participants[idx];
         p['offed'] = p.offed == 0 ? 1 : 0;
 
-        fetch(`/api/tournament/${tournament.id}/participant/${p.id}/`, 
-            { method: 'PUT', 'credentials': 'same-origin',
-              headers: 
-              {
-                'Content-Type': 'application/json',
-                "X-CSRFToken": getCookie("csrftoken")
-              },
-              body: JSON.stringify(p)
-        }).then(resp => resp.json()).then(json => dispatch({type: 'editParticipant', participant: json}))
+        fetch(`/api/tournament/${tournament.id}/participant/${p.id}/`,
+            {
+                method: 'PUT', 'credentials': 'same-origin',
+                headers:
+                {
+                    'Content-Type': 'application/json',
+                    "X-CSRFToken": getCookie("csrftoken")
+                },
+                body: JSON.stringify(p)
+            }).then(resp => resp.json()).then(json => dispatch({ type: 'editParticipant', participant: json }))
     }
 
     /**
@@ -42,73 +56,112 @@ export function Participants(props) {
      */
     function deleteParticipant(e, idx) {
         const p = tournament.participants[idx];
-        fetch(`/api/tournament/${tournament.id}/participant/${p.id}/`, 
-            { method: "DELETE", 'credentials': 'same-origin',
-              headers: 
-              {
-                'Content-Type': 'application/json',
-                "X-CSRFToken": getCookie("csrftoken")
-              },
-              body: JSON.stringify(p)
-        }).then(resp => {
-             if(resp.ok) {
-                dispatch({type: 'deleteParticipant', participant: p})
-            }
-        })
+        fetch(`/api/tournament/${tournament.id}/participant/${p.id}/`,
+            {
+                method: "DELETE", 'credentials': 'same-origin',
+                headers:
+                {
+                    'Content-Type': 'application/json',
+                    "X-CSRFToken": getCookie("csrftoken")
+                },
+                body: JSON.stringify(p)
+            }).then(resp => {
+                if (resp.ok) {
+                    dispatch({ type: 'deleteParticipant', participant: p })
+                }
+            })
     }
 
     function isStarted() {
         return tournament.rounds[0].paired
     }
+
+    function changeOrder(field) {
+        if(field == order || `-${field}` == order) {
+            setOrder(`-${field}`)    
+            tournament.participants.reverse()
+            dispatch({type: 'participants', participants: [...tournament.participants]})
+        }
+        else {
+            if(field === 'name') {
+                tournament.participants.sort( (a,b) => a.name.localeCompare(b.name))
+            }
+            else {
+                tournament.participants.sort( (a,b) => a[field] - b[field])
+            }
+            setOrder(field)
+            dispatch({type: 'participants', participants: [...tournament.participants]})
+        }
+        
+    }
+
+    function getHeading(name, field) {
+        if (order == field) {
+            return (
+                <th onClick={e => changeOrder(field)} key={field}>
+                    {name}<i className='bi-sort-down-alt ml-2'></i>
+                </th>)
+        }
+        if (order == `-${field}`) {
+            return (
+                <th onClick={e => changeOrder(field)}  key={field}>
+                    {name}<i className='bi-sort-up-alt ml-2'></i>
+                </th>)
+        }
+        return (
+            <th onClick={e => changeOrder(field)}  key={field}>
+                {name}
+            </th>)
+    }
+
     return (
-       
         <table className='table table-striped align-middle table-bordered table-dark'>
-          <thead>
-            <tr>
-              <td>Rank</td>
-              <td className="text-left">Name</td>
-              <td className="text-right">rating</td>
-              <td className="text-right">Round Wins</td>
-              <td className="text-right">Game Wins</td>
-              <td className="text-right">Spread</td>
-              { auth && <td className="text-right">Actions</td> }
-            </tr>
-          </thead>
-          <tbody>
-            { tournament?.participants?.map((row, idx) => (
-              <tr
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <td className="text-left">{ idx + 1}</td>
-                <td component="th" scope="row">
-                    <Link to={ `${row.id}` }>{row.name}</Link>
-                </td>
-                <td className="text-right">{row.rating}</td>
-                <td className="text-right">{row.round_wins}</td>
-                <td className="text-right">{row.game_wins}</td>
-                <td className="text-right">{row.spread}</td>
-                {auth && 
-                    <td className="text-right">
-                        <div className='d-inline-flex'>
-                            <div className='form-check form-switch'>
-                                <input className="form-check-input" type="checkbox"
-                                    checked={row.offed != 0} onChange={ e => toggleParticipant(e, idx) }/>
-                            </div>
-                            { !isStarted() &&
-                                <div className='form-col'>
-                                    <i className='bi-x-circle' onClick = { e => deleteParticipant(e, idx)}></i>
+            <thead>
+                <tr>
+                    {columns.map(col => {
+                        if (col.length == 2) {
+                            return getHeading(col[0], col[1]);
+                        }
+                        return <th key={col[0]}>{col[0]}</th>
+                    }
+                    )}
+                </tr>
+            </thead>
+            <tbody>
+                {tournament?.participants?.map((row, idx) => (
+                    <tr
+                        key={row.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <td className="text-left">{ row.pos }</td>
+                        <td component="th" scope="row">
+                            <Link to={`${row.id}`}>{row.name}</Link>
+                        </td>
+                        <td className="text-right">{row.rating}</td>
+                        <td className="text-right">{row.round_wins}</td>
+                        <td className="text-right">{row.game_wins}</td>
+                        <td className="text-right">{row.spread}</td>
+                        {auth &&
+                            <td className="text-right">
+                                <div className='d-inline-flex'>
+                                    <div className='form-check form-switch'>
+                                        <input className="form-check-input" type="checkbox"
+                                            checked={row.offed != 0} onChange={e => toggleParticipant(e, idx)} />
+                                    </div>
+                                    {!isStarted() &&
+                                        <div className='form-col'>
+                                            <i className='bi-x-circle' onClick={e => deleteParticipant(e, idx)}></i>
+                                        </div>
+                                    }
                                 </div>
-                            }
-                        </div>
-                    </td>
-                }
-              </tr>
-            ))}
-          </tbody>
+                            </td>
+                        }
+                    </tr>
+                ))}
+            </tbody>
         </table>
-      
-     );
+
+    );
 }
 
 export function Participant(props) {
@@ -117,18 +170,18 @@ export function Participant(props) {
     const tournament = useTournament()
 
     useEffect(() => {
-        if(participant == null && tournament) {
-            fetch(`/api/tournament/${tournament.id}/participant/${params.id}/`).then(resp=>resp.json()).then(json=>{
+        if (participant == null && tournament) {
+            fetch(`/api/tournament/${tournament.id}/participant/${params.id}/`).then(resp => resp.json()).then(json => {
                 setParticipant(json)
             })
-        } 
-    },[tournament, participant])
+        }
+    }, [tournament, participant])
 
     function editScore(e) {
 
     }
 
-    if(participant) {
+    if (participant) {
         return (
             <div>
                 <h2><Link to={`/${tournament.slug}`}>{tournament.name}</Link></h2>
@@ -143,14 +196,14 @@ export function Participant(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        { participant?.members?.map(p => (
+                        {participant?.members?.map(p => (
                             <tr key={p.id}>
                                 <td>{p.board}</td>
                                 <td>{p.name}</td>
                                 <td>{p.wins}</td>
                                 <td>{p.spread}</td>
                             </tr>
-                            )
+                        )
                         )}
                     </tbody>
                 </table>
@@ -162,3 +215,5 @@ export function Participant(props) {
     }
     return <div></div>
 }
+
+console.log('Participant 0.01')
