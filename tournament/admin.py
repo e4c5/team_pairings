6ad81451df0102366a86c05a308fc57023ba1118
raditha <raditha.dissanayake@gmail.com>
2023-06-14 07:53:39 +0530

    It still amazes me what just a few lines of code can achieve in admin

--- tournament/admin.py
+++ tournament/admin.py
@@ -7,24 +7,26 @@ from tournament.models import Tournament, TournamentRound, Participant, \
 
 class BoardResultAdmin(admin.ModelAdmin):
     list_display = ['id', 'round','team1','team2','board','score1','score2']
-
+    raw_id_fields = ['round', 'team1', 'team2']
 
 class ResultAdmin(admin.ModelAdmin):
     list_display = ['id', 'round','p1','p2','games_won','score1','score2']
-
-
+    raw_id_fields = ['round', 'p1', 'p2','starting']
+    
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ['id','name','num_rounds','entry_mode']
 
 class RoundAdmin(admin.ModelAdmin):
     list_display = ['tournament', 'round_no','spread_cap','pairing_system',
         'repeats','based_on']
+    search_fields = ['tournament__name']
 
 class TDAdmin(admin.ModelAdmin):
     list_display = ['tournament', 'user']
 
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ['tournament','name','seed','round_wins','game_wins']
+    list_display = ['pk', 'tournament','name','seed','round_wins','game_wins']
+    search_fields = ['tournament__name', 'name']
 
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ['team','board','name','wins','spread']
