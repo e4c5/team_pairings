787be23496ad7e744eac40f0b1d0fd1b092e3390
raditha <raditha.dissanayake@gmail.com>
2023-08-27 18:43:53 +0530

    Tiny weeny details

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from profiles.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('national_list_name', 'wespa_list_name', 'preferred_name', 'beginner')
+    list_display = ('user', 'national_list_name', 'wespa_list_name', 'preferred_name', 'beginner')
     
 admin.site.register(Profile, ProfileAdmin)
  
